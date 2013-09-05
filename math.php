d7a203b80c4c636a54197c15567541d9a9c6e309
Amyr Haq <amyr@amyrhaq.com>
2013-09-04 21:15:19 -0700

    Initial commit

---
+++ math.php
@@ -0,0 +1,22 @@
+<?php
+
+    $x = 10;
+    $y = 2;
+
+    //Addition
+    echo "$x + $y = \n";
+    echo $x+$y;
+
+    //Subtraction
+    echo "\n\n$x - $y = \n";
+    echo $x-$y;
+
+    //Multiplication
+    echo "\n\n$x x $y = \n";
+    echo $x*$y;
+
+    //Division
+    echo "\n\n$x / $y = \n";
+    echo $x/$y;
+
+?>
